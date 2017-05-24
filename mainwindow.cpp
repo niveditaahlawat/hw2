11c95faece3aaa3426df87ee39d352ad8f12e29d
niveditaahlawat <niveditaahlawat@ucla.edu>
2017-05-24 10:20:19 -0700

    connected homework spin boxes to sliders
    I connected the spin boxes of each homework to its corresponding slider.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,53 +19,79 @@ void MainWindow::on_hw1_slider_valueChanged(int value)
 {
     ui->hw1_score->setValue(value);
 }
-
 void MainWindow::on_hw2_slider_valueChanged(int value)
 {
     ui->hw2_score->setValue(value);
 }
-
 void MainWindow::on_hw3_slider_valueChanged(int value)
 {
     ui->hw3_score->setValue(value);
 }
-
 void MainWindow::on_hw4_slider_valueChanged(int value)
 {
     ui->hw4_score->setValue(value);
 }
-
 void MainWindow::on_hw5_slider_valueChanged(int value)
 {
     ui->hw5_score->setValue(value);
 }
-
 void MainWindow::on_hw6_slider_valueChanged(int value)
 {
     ui->hw6_score->setValue(value);
 }
-
 void MainWindow::on_hw7_slider_valueChanged(int value)
 {
     ui->hw7_score->setValue(value);
 }
-
 void MainWindow::on_hw8_slider_valueChanged(int value)
 {
     ui->hw8_score->setValue(value);
 }
 
+
 void MainWindow::on_mid1slider_valueChanged(int value)
 {
     ui->mid1_score->setValue(value);
 }
-
 void MainWindow::on_mid2slider_valueChanged(int value)
 {
     ui->mid2_score->setValue(value);
 }
-
 void MainWindow::on_finalslider_valueChanged(int value)
 {
     ui->final_score->setValue(value);
+}
+
+
+void MainWindow::on_hw1_score_valueChanged(double arg1)
+{
+    ui->hw1_slider->setValue(arg1);
+}
+void MainWindow::on_hw2_score_valueChanged(double arg1)
+{
+    ui->hw2_slider->setValue(arg1);
+}
+void MainWindow::on_hw3_score_valueChanged(double arg1)
+{
+    ui->hw3_slider->setValue(arg1);
+}
+void MainWindow::on_hw4_score_valueChanged(double arg1)
+{
+    ui->hw4_slider->setValue(arg1);
+}
+void MainWindow::on_hw5_score_valueChanged(double arg1)
+{
+    ui->hw5_slider->setValue(arg1);
+}
+void MainWindow::on_hw6_score_valueChanged(double arg1)
+{
+    ui->hw6_slider->setValue(arg1);
+}
+void MainWindow::on_hw7_score_valueChanged(double arg1)
+{
+    ui->hw7_slider->setValue(arg1);
+}
+void MainWindow::on_hw8_score_valueChanged(double arg1)
+{
+    ui->hw8_slider->setValue(arg1);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -31,6 +31,15 @@ private slots:
     void on_mid2slider_valueChanged(int value);
     void on_finalslider_valueChanged(int value);
 
+    void on_hw1_score_valueChanged(double arg1);
+    void on_hw2_score_valueChanged(double arg1);
+    void on_hw3_score_valueChanged(double arg1);
+    void on_hw4_score_valueChanged(double arg1);
+    void on_hw5_score_valueChanged(double arg1);
+    void on_hw6_score_valueChanged(double arg1);
+    void on_hw7_score_valueChanged(double arg1);
+    void on_hw8_score_valueChanged(double arg1);
+
 private:
     Ui::MainWindow *ui;
 };
